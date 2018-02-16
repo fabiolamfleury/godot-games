eccdd3e8584ea0ead4f5456e5cfd09215958d722
Fabíola Fleury <fabiola.mfleury@gmail.com>
2018-02-16 19:14:37 -0200

    Adding initial assets and character animation

---
+++ TimberFelpudo/scripts/felpudo.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+onready var idle = get_node("Idle")
+onready var hit = get_node("Hit")
+onready var grave = get_node("Grave")
+onready var animation = get_node("Animation")
+
+var side;
+const LEFT = 0
+const RIGHT = 1
+func _ready():
+	idle.show()
+	hit.hide()
+	pass
+func left():
+	set_pos(Vector2(220, 1070))
+	side = LEFT
+	idle.set_flip_h(false)
+	hit.set_flip_h(false)
+	grave.set_pos(Vector2(-44, 41))
+	grave.set_flip_h(true)
+	
+func right():
+	set_pos(Vector2(500, 1070))
+	side = RIGHT
+	idle.set_flip_h(true)
+	idle.set_flip_h(true)
+	grave.set_pos(Vector2(28, 41))
+	grave.set_flip_h(false)
+
+func hitting():
+	animation.play("Hitting")
